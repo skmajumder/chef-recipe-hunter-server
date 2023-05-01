a227c3dbbeefb41dbdeefde56a1f5b4810bc28eb
skmajumder <skmajumder.cse@gmail.com>
2023-05-02 02:39:29 +0600

    Show recipes for chef

--- index.js
+++ index.js
@@ -6,8 +6,31 @@ const port = 3000;
 
 app.use(cors());
 
+const chefs = require("./data/chef.json");
+const recipes = require("./data/cuisine.json");
+
 app.get("/", (req, res) => {
   res.send("Express server is running at http://localhost:3000");
+});
+
+app.get("/recipes", (req, res) => {
+  res.send(recipes);
+});
+
+app.get("/chef", (req, res) => {
+  res.send(chefs);
+});
+
+app.get("/chef/:id", (req, res) => {
+  const chefId = req.params.id;
+  console.log(chefId);
+
+  const chefRecipesIds = chefs.find((chef) => chef.id == chefId)?.num_recipes;
+  
+  const chefRecipes = recipes.filter((recipe) =>
+    chefRecipesIds?.includes(recipe.recipe_id)
+  );
+  res.send(chefRecipes);
 });
 
 app.listen(port, () => {
